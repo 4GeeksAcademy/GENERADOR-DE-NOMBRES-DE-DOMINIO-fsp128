5578f642746ea6520d95005435fcddddf1f65904
juan jose abreu goncalves <juanjoseabreu23@gmail.com>
2025-11-24 20:10:19 +0000

    generador-terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our', 'the'];
+let adj = ['great', 'big', 'last'];
+let noun = ['jogger', 'racoon', 'ofus'];
+let terminacion = ['com', 'net', 'us', 'io', 'es']
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            for (let l = 0; l < terminacion.length; l++) {
+                let dominio = pronoun[i]+adj[j]+noun[k]
+                if (dominio.endsWith(terminacion[l])) {
+                    console.log(dominio.slice(0,(dominio.length - terminacion[l].length)) + "." + terminacion[l])
+                } else {
+                    console.log(dominio + "." + terminacion[l])
+                }
+            }
+        }
+    }
+}
